389bf08d09c51c274570fe7e348b1d80c5d33c7f
Yuta Takahashi <m1205123ing@gmail.com>
2017-12-16 09:04:41 +0900

    devide into search_api.rb

---
+++ lib/gcs_api_gem/search_api.rb
@@ -0,0 +1,59 @@
+module GcsApiGem
+  # SearchApi
+  class SearchApi
+    class << self
+      def image(keyword)
+        return { error: 'Please set KEY to environment variable.' } if blank?(ENV['KEY'])
+        return { error: 'Please set CSE_ID to environment variable.' } if blank?(ENV['CSE_ID'])
+
+        choice(fetch(keyword))
+      end
+
+      private
+
+      def url
+        'https://www.googleapis.com/customsearch/v1'
+      end
+
+      def query(keyword)
+        {
+          'key' => ENV['KEY'],
+          'cx' => ENV['CSE_ID'],
+          'searchType' => 'image',
+          'q' => keyword,
+          'start' => rand(50) + 1
+        }
+      end
+
+      def fetch(keyword)
+        client = HTTPClient.new
+        res = client.get(url, query(keyword))
+        JSON.parse res.body
+      end
+
+      def choice(res)
+        if blank?(res['error'])
+          if res['searchInformation']['totalResults'].to_i.zero?
+            return { error: 'Search result was 0.' }
+          end
+          formatted_item(res)
+        else
+          res['error']
+        end
+      end
+
+      def blank?(obj)
+        obj.nil? || obj.empty?
+      end
+
+      def formatted_item(res)
+        item = res['items'][rand(10)]
+        {
+          title: item['title'],
+          image: item['link'],
+          search_result: res['searchInformation']['totalResults']
+        }
+      end
+    end
+  end
+end
